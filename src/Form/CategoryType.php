1998b857bd6d3e9a67b344261135cd53be13c39c
Diego Bastidas <diegofernando150@gmail.com>
2020-06-01 11:38:50 -0500

    crud category

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('photo', FileType::class, [
+                'label'=>'Seleccione una foto para la categoría',
+                'mapped' => false
+            ])
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
