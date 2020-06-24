ffd97fc800f0c2324f08056cd2afc20016baff33
Diego Bastidas <diegofernando150@gmail.com>
2020-06-24 09:44:23 -0500

    Epayco II

--- src/Controller/ShoppingCarController.php
+++ src/Controller/ShoppingCarController.php
@@ -149,6 +149,51 @@ class ShoppingCarController extends AbstractController
      * @Route("/epayco/success/", name="epayco_success")
      */
     public function EpaycoSuccess(){
+
+        $p_cust_id_cliente = '80247';
+        $p_key             = '1d83674120c9afc3f2bc0244c1fb4f5702991755';
+
+        $x_ref_payco      = $_REQUEST['x_ref_payco'];
+        $x_transaction_id = $_REQUEST['x_transaction_id'];
+        $x_amount         = $_REQUEST['x_amount'];
+        $x_currency_code  = $_REQUEST['x_currency_code'];
+        $x_signature      = $_REQUEST['x_signature'];
+
+
+
+        $signature = hash('sha256', $p_cust_id_cliente . '^' . $p_key . '^' . $x_ref_payco . '^' . $x_transaction_id . '^' . $x_amount . '^' . $x_currency_code);
+
+        $x_response     = $_REQUEST['x_response'];
+        $x_motivo       = $_REQUEST['x_response_reason_text'];
+        $x_id_invoice   = $_REQUEST['x_id_invoice'];
+        $x_autorizacion = $_REQUEST['x_approval_code'];
+
+//Validamos la firma
+        if ($x_signature == $signature) {
+            /*Si la firma esta bien podemos verificar los estado de la transacción*/
+            $x_cod_response = $_REQUEST['x_cod_response'];
+            switch ((int) $x_cod_response) {
+                case 1:
+                    # code transacción aceptada
+                    echo "transacción aceptada";
+                    break;
+                case 2:
+                    # code transacción rechazada
+                    echo "transacción rechazada";
+                    break;
+                case 3:
+                    # code transacción pendiente
+                    echo "transacción pendiente";
+                    break;
+                case 4:
+                    # code transacción fallida
+                    echo "transacción fallida";
+                    break;
+
+            }
+        } else {
+            die("Firma no valida");
+        }
         return $this->render('shopping_car/epayco_success.html.twig');
     }
 
