1a02cf28f8d72a974f373c7e96cc69dfcdb7b57b
Diego Bastidas <diegofernando150@gmail.com>
2020-07-02 17:32:21 -0500

    interfaz gráfica de usuario parte II

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -7,6 +7,9 @@ use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,16 +18,17 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class, ['label'=>'Nombre del producto'])
+            ->add('description', TextareaType::class, ['label'=>'Descripción del producto'])
             ->add('photo', FileType::class, [
                 'label'=>'Seleccione una foto para el producto',
                 'mapped' => false,
                 'required'=>false
             ])
-            ->add('price')
-            ->add('discount')
+            ->add('price', IntegerType::class, ['label'=>'Precio del producto'])
+            ->add('discount', IntegerType::class,['label'=>'Descuento del producto (Porcentaje)'])
             ->add('category', EntityType::class, [
+                'label'=>'Categoría',
                 'class'=>Category::class,
                 'choice_label' => 'name',
 
